d5c4880ffa638bec74c11773deb01ce0a9770517
Vinicius Oliveira <50337315+vinicius9141@users.noreply.github.com>
2022-10-24 16:12:52 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'toDoList';
+export class AppComponent implements OnInit {
+  public newTask: any;
+  public refreshedTasks: any;
+
+  public tasks: any = [];
+
+  ngOnInit() {
+    this.readTasks();
+  }
+
+  public addTask(): void {
+    if (!this.newTask) return alert('o buro preenche ai');
+    this.tasks.push(this.newTask);
+    this.saveTasks();
+    this.newTask = '';
+  }
+
+  public readTasks(): void {
+    this.tasks = JSON.parse(localStorage.getItem('task')!) || [];
+  }
+
+  public removeTask(index: number): void {
+    this.tasks.splice(index, 1);
+    this.saveTasks();
+  }
+
+  public saveTasks(): void {
+    localStorage.setItem('task', JSON.stringify(this.tasks));
+  }
 }
